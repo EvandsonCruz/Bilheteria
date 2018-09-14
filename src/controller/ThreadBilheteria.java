657405ad4e148121ac4a28fa86d2fabd8af00fa9
EvandsonCruz <evandson.cruz@outlook.com>
2018-09-14 15:37:34 -0300

    Primeira att

--- src/controller/ThreadBilheteria.java
+++ src/controller/ThreadBilheteria.java
@@ -3,56 +3,58 @@ package controller;
 import java.util.Random;
 import java.util.concurrent.Semaphore;
 
-public class ThreadBilheteria extends Thread{
-	
+public class ThreadBilheteria extends Thread {
+
 	Random random = new Random();
 	private Semaphore semaforo;
 	private static int bilhetes = 100;
-	
+	private boolean verificaCompra=false;
+
 	public ThreadBilheteria(Semaphore semaforo) {
 		this.semaforo = semaforo;
 	}
-	
+
 	public void run() {
-		if(logar()){
-			
-		}
-		else{
-			if(comprar()){
-				
+		while (bilhetes > 0 && verificaCompra==false) {
+			if (logar()) {
+
+			} else {
+				if (comprar()) {
+
+				} else {
+					validar();
+				}
 			}
-			else{
-				validar();
-			}
 		}
-	}
-	
-	public boolean logar(){
-		int tempo=0;
-		tempo = random.nextInt(1950)+50;
-		if(tempo > 1000){
-			System.out.println("Timeout");
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-	
-	public boolean comprar(){
-		int tempo=0;
-		tempo = random.nextInt(2000)+1000;
-		if(tempo > 2500){
-			System.out.println("Final de tempo de sessão");
-			return true;
-		}
-		else{
-			return false;
-		}
-	}
-	
-	public void validar(){
-		
 	}
 
+	public boolean logar() {
+		int tempo = 0;
+		tempo = random.nextInt(1950) + 50;
+		if (tempo > 1000) {
+			System.out.println("Timeout");
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean comprar() {
+		int tempo = 0;
+		tempo = random.nextInt(2000) + 1000;
+		if (tempo > 2500) {
+			System.out.println("Final de tempo de sessão");
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void validar() {
+		int qtd;
+		qtd = random.nextInt(3)+1;
+		if(qtd >= bilhetes){
+			bilhetes-=qtd;
+		}
+	}
 }
